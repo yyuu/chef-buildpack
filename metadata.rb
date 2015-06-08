a6021c21052a4a3dd4f80ad36148901e2a9f35ea
Yamashita Yuu <peek824545201@gmail.com>
2015-06-08 18:10:57 +0900

    Create repository

---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name             'buildpack'
+maintainer       'YOUR_NAME'
+maintainer_email 'YOUR_EMAIL'
+license          'All rights reserved'
+description      'Installs/Configures buildpack'
+long_description 'Installs/Configures buildpack'
+version          '0.1.0'
