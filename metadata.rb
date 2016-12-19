58601b790f52805f55ee37738b0463d07b1f58e1
Yamashita, Yuu <peek824545201@gmail.com>
2016-12-19 03:58:34 +0000

    Add `source_url` in metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -1,6 +1,7 @@
 name             'buildpack'
 maintainer       'Yamashita, Yuu'
 maintainer_email 'yuu@treasure-data.com'
+source_url       'https://github.com/yyuu/chef-buildpack'
 license          'Apache 2.0'
 description      'Installs/Configures application with using buildpack'
 long_description 'Installs/Configures application with using buildpack'
